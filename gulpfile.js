11ed4088ddc2e1b5ef6c4266d1145bb6a2c8c2d9
muryliang <muryliang@gmail.com>
2020-08-19 23:19:05 +0800

    currently fail image compress, when success, need add at bottom

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,49 @@
+// 引入需要的模块
 var gulp = require('gulp');
-
-
 var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-//压缩css
-gulp.task('minify-css', function () {
-return gulp.src('./public/**/*.css')
-.pipe(minifycss())
-.pipe(gulp.dest('./public'));
-});
-//压缩html
-gulp.task('minify-html', function () {
-return gulp.src('./public/**/*.html')
-.pipe(htmlclean())
-.pipe(htmlmin({
-removeComments: true,
-minifyJS: true,
-minifyCSS: true,
-minifyURLs: true,
-}))
+var imagemin = require('gulp-imagemin');
 
-.pipe(gulp.dest('./public'))
-});
-//压缩js 不压缩min.js
-gulp.task('minify-js', function () {
-return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-.pipe(uglify())
-.pipe(gulp.dest('./public'));
+// 压缩public目录下所有html文件, minify-html是任务名, 设置为default，启动gulp压缩的时候可以省去任务名
+gulp.task('minify-html', function() {
+    return gulp.src('./public/**/*.html') // 压缩文件所在的目录
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public')) // 输出的目录
 });
 
-//4.0以前的写法
-//gulp.task('default', [
-  //  'minify-html', 'minify-css', 'minify-js'
-//]);
-//4.0以后的写法
-// 执行 gulp 命令时执行的任务
-gulp.task('default', gulp.parallel('minify-html', 'minify-css', function() {
-    console.log("----------gulp Finished----------");
-  // Do something after a, b, and c are finished.
-}));
+// 压缩css
+gulp.task('minify-css', function() {
+    return gulp.src(['./public/**/*.css','!./public/js/**/*min.css'])
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('./public'));
+});
+// 压缩js
+gulp.task('minify-js', function() {
+    return gulp.src(['./public/**/.js','!./public/js/**/*min.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
+});
+// 压缩图片
+//gulp.task('minify-images', function() {
+//    return gulp.src(['./public/**/*.png','./public/**/*.jpg','./public/**/*.gif'])
+//        .pipe(imagemin(
+//        [imagemin.gifsicle({'optimizationLevel': 3}), 
+//        imagemin.mozjpeg({'progressive': true}), 
+//        imagemin.optipng({'optimizationLevel': 5}), 
+//        imagemin.svgo()],
+//        {'verbose': true}))
+//        .pipe(gulp.dest('./public'))
+//});
+// gulp 4.0 适用的方式
+gulp.task('default', gulp.parallel('minify-html','minify-css','minify-js'
+));
